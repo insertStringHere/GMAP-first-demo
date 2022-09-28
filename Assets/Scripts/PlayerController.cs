d2566c23e646a244704a630550166c2f0e7fb0a5
imf22 <92887508+imf22@users.noreply.github.com>
2022-09-28 17:24:36 -0400

    Added First Person Mouse Control
    Added free look first person mouse controller
    Adjust sensitivity via inspector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,37 +13,49 @@ public class PlayerController : MonoBehaviour
     private float gravityValue = -9.81f;
 
     public Transform cam;
+    private Vector2 turn;
     public float turnSmoothnessTime = 0.1f;
+    [Range(0.5f, 3)] public float mouseXSensitiviy = 1;
+    [Range(0.5f, 3)] public float mouseYSensitiviy = 1;
     private float turnSmoothSpeed;
 
 
-    //private void Start()
-    //{
-    //    controller = gameObject.AddComponent<CharacterController>();
-    //}
+    private void Start()
+    {
+        //controller = gameObject.AddComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
 
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movementDirection = new Vector3(horizontal, 0f, vertical).normalized;
+
+        turn.x += Input.GetAxis("Mouse X") * mouseXSensitiviy;
+        turn.y += Input.GetAxis("Mouse Y") * mouseYSensitiviy;
+        transform.rotation = Quaternion.Euler(-turn.y, turn.x, 0f);
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 movementDirection = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (movementDirection.magnitude >= 0.1f)
         {
             // Face character in correct direction
             float directionAngle = Mathf.Atan2(movementDirection.x, movementDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, directionAngle, ref turnSmoothSpeed, turnSmoothnessTime);
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, smoothAngle, 0f);
 
             // Convert rotation to vector by multiplying by normal of z axis
             Vector3 moveDirectionCam = Quaternion.Euler(0f, directionAngle, 0f) * Vector3.forward;
             controller.Move(moveDirectionCam.normalized * playerSpeed * Time.deltaTime);
+
+            
         }
 
         //playerVelocity.Set(Input.GetAxis("Horizontal") * playerSpeed, playerVelocity.y, Input.GetAxis("Vertical") * playerSpeed);
