a2157228bced15ec3ae91e6bfdd58cb66679d353
ChristianLGameDesigner <christian.locantore@gmail.com>
2022-10-26 13:27:46 -0400

    Adding comments to code I wrote and deleting other
    Added comments to the SpawningRocks, FallingRocksObstacle, and TriggerWalls script. Deleted the triggerwall code from PhysicsPlayerController as it is now on the TriggerWalls script.

--- Assets/Scripts/SpawningRocks.cs
+++ Assets/Scripts/SpawningRocks.cs
@@ -5,29 +5,25 @@ using UnityEngine;
 public class SpawningRocks : MonoBehaviour
 {
     public GameObject rockPrefab;
-    void Start()
+    void Start() 
     {
         StartCoroutine(SpawnFalling());
     }
 
-    IEnumerator SpawnFalling()
+    IEnumerator SpawnFalling() //Put this script on an empty game object to spawn rocks from it
     {
         while (true)
         {
-            float randomTime = Random.Range(1f, 2f);
-            float setPosition = Random.Range(-50f, -30f);
+            float randomTime = Random.Range(1f, 2f); //time it waits between spawning new prefabs
+            float setPosition = Random.Range(-50f, -30f); //spawns prefabs in range for x position depending on where you place the empty game object
             Instantiate(rockPrefab, new Vector3(setPosition, transform.position.y, transform.position.z), Quaternion.identity);
             yield return new WaitForSeconds(randomTime);
-            Instantiate(rockPrefab, new Vector3(setPosition, transform.position.y, transform.position.z), Quaternion.identity);
+            Instantiate(rockPrefab, new Vector3(setPosition, transform.position.y, transform.position.z), Quaternion.identity); //repeated to spawn multiple at the same time
             float randomTime2 = Random.Range(1f, 3f);
-            float setPosition2 = Random.Range(-30f, -80f);
+            float setPosition2 = Random.Range(-30f, -80f); //different set of numbers to spawn other rocks at the same time but in a different location
             Instantiate(rockPrefab, new Vector3(setPosition2, transform.position.y, transform.position.z-2), Quaternion.identity);
             yield return new WaitForSeconds(randomTime2);
             Instantiate(rockPrefab, new Vector3(setPosition2, transform.position.y, transform.position.z+2), Quaternion.identity);
         }
-    }
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/TriggerWalls.cs
+++ Assets/Scripts/TriggerWalls.cs
@@ -23,6 +23,13 @@ public class TriggerWalls : MonoBehaviour
                 }
             }
         }
+
+        ///<summary>
+        /// This script is for triggers, when player enter triggers it will set other obstacles active or not(opening, or closing, different passageways) 
+        ///</summary>
+
+
+
         // if (other.gameObject.CompareTag("TWall"))
         // {
         //     other.gameObject.SetActive(false);
@@ -38,12 +45,12 @@ public class TriggerWalls : MonoBehaviour
     // // Start is called before the first frame update
     // void Start()
     // {
-        
+
     // }
 
     // // Update is called once per frame
     // void Update()
     // {
-        
+
     // }
 }
