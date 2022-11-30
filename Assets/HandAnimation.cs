698f1f531f1fc100bd1354cca9188d2d472f72f0
imf22 <92887508+imf22@users.noreply.github.com>
2022-11-29 21:58:12 -0500

    Added Time Rewinder Effect
    Rewinding enables a rewinding particle

--- Assets/HandAnimation.cs
+++ Assets/HandAnimation.cs
@@ -6,6 +6,9 @@ public class HandAnimation : MonoBehaviour
 {
     public Animator timeRewindAnimator;
     public float transitionTime = 1;
+    public GameObject goodParticlePrefab;
+    public GameObject badParticlePrefab;
+    //public GameObject timeTurnerAnimationObject;
     public KeyCode rewindPrimary = KeyCode.Mouse0;
     public KeyCode rewindSecondary = KeyCode.Q;
 
@@ -14,10 +17,23 @@ public class HandAnimation : MonoBehaviour
         if(Input.GetKey(rewindPrimary) || Input.GetKey(rewindSecondary))
         {
             timeRewindAnimator.SetBool("Open", true);
+            Invoke("setParticlesActive", 0.55f);
         }
         else
         {
             timeRewindAnimator.SetBool("Open", false);
+            goodParticlePrefab.SetActive(false);
+            badParticlePrefab.SetActive(false);
         }
+
     }
+
+    // Set in method as to invoke after delay
+    private void setParticlesActive()
+    {
+        goodParticlePrefab.SetActive(true);
+    }
+
+
+
 }
